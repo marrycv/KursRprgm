897d9cc526ef8201228797042b90399ed26e2b22
Måns Magnusson <mons.magnusson@gmail.com>
2014-01-14 16:29:36 +0100

    LabD4 utkast1

---
+++ Labs/Files/LabD4.R
@@ -0,0 +1,37 @@
+#### Programmering i R ####
+#### Inlämning, Datalaboration 3 ####
+
+Namn <- "Age ditt namn här"
+LiuId <- "Age ditt LIU-ID här"
+
+
+# Uppgift 1 : pnrFormat() ----
+pnrFormat <- function(pnr){
+  # Skriv din kod här
+}
+
+# Uppgift 2 : pnrCtrl() ---- 
+pnrCtrl <- function(pnr){
+  # Skriv din kod här
+}
+
+# Uppgift 3 : pnrSex() ---- 
+pnrSex <- function(pnr){
+  # Skriv din kod här
+}
+
+# Uppgift 4 : pnrSamordn() ----
+pnrSamordn <- function(pnr) {
+  # Skriv din kod här
+}
+
+# Uppgift 5 : pnrAge() ----
+pnrAge <- function(pnr, date) {
+  # Skriv din kod här
+}
+
+# Uppgift 6 : pnrInfo() ----
+pnrInfo <- function(pnr,...){
+  # Skriv din kod här
+}
+

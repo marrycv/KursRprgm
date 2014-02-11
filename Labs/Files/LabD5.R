2a47c9e3ad2fd1d9ad5932b16c7a2018f433aa99
Måns Magnusson <mons.magnusson@gmail.com>
2014-02-11 15:38:36 +0100

    Labbmall D5

---
+++ Labs/Files/LabD5.R
@@ -0,0 +1,52 @@
+#--------------------------------------------------------------------
+#### Programmering i R ####
+#### Inlämning, Datalaboration 5 ####
+#--------------------------------------------------------------------
+
+# Ange gruppnummer här
+GruppNr "Mitt gruppnummer (ex. 4)"
+# Ange namnen här för alla i gruppen, ex Namn <-c( "Alice Walker","Gustav Vasa")
+Namn <- c("Mitt namn (ex. Måns Magnusson)", "Mitt namn (ex. Josef Wilzén)")
+# Ange Liu-ID här för alla i gruppen, ex LiuId <- c("aliwa432","gusva204")
+LiuId  <- c("Mitt LiuId (ex. manma97)", "Mitt LiuId (ex. joswi128)")
+
+
+
+#--------------------------------------------------------------------
+# Uppgift: Beskrivande statistik
+#--------------------------------------------------------------------
+
+findNumeric<-function(myData){
+  # Skriv din kod här
+  return()
+}
+
+
+findCharacter<-function(myData){
+  # Skriv din kod här
+  return()
+}
+
+
+orderMyData<-function(myData){
+  # Skriv din kod här
+  return()
+}
+
+
+myStatsFunc <- function(){
+  # Skriv din kod här
+  return()
+}
+
+
+
+#--------------------------------------------------------------------
+# Uppgift: Kontrollera en plot
+#--------------------------------------------------------------------
+
+subPlotFunc<-function(myData,rowLength,colLength,myOrder){
+  # Skriv din kod här
+}
+
+

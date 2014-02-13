c474cbeaa445e8ba2927478f2d7fc960d914aab0
Måns Magnusson <mons.magnusson@gmail.com>
2014-02-13 22:57:18 +0100

    Update babylon tests

--- Labs/Test/babylonTest.R
+++ Labs/Test/babylonTest.R
@@ -9,15 +9,15 @@ test_that("Assignment: babylon()", {
               info = "Fel: Funktionen returnerar inte en lista.")
   expect_that(all(names(babylon(x=2, init=1.5, tol=0.01)) %in% c("iter", "rot")), condition=is_true(),
               info = "Fel: Funktionen returnerar inte en med rätt namngivna element.")
-  expect_equal(babylon(x=2, init=1.5, tol=0.01)$rot, 1.414216, tolerance = .001,
+  expect_equal(babylon(x=2, init=1.5, tol=0.01)$rot, 1.414216, tolerance = .01,
               info = "Fel: Approximationen är felaktig för x = 2, init = 1.5 och tol = 0.01.")
   expect_equal(babylon(x=2, init=1.5, tol=0.01)$iter, 2,
                info = "Fel: Antalet iterationer är felaktig för x = 2, init = 1.5 och tol = 0.01.")  
-  expect_equal(babylon(x=15, init=1.5, tol=0.01)$rot, 3.872983, tolerance = .001,
+  expect_equal(babylon(x=15, init=1.5, tol=0.01)$rot, 3.872, tolerance = .01,
                info = "Fel: Approximationen är felaktig för x = 15, init = 1.5 och tol = 0.01.")
   expect_equal(babylon(x=15, init=1.5, tol=0.01)$iter, 5,
                info = "Fel: Antalet iterationer är felaktig för x = 15, init = 1.5 och tol = 0.01.")
-  expect_equal(babylon(x=15, init=0.1, tol=0.01)$rot, 3.872983, tolerance = .001,
+  expect_equal(babylon(x=15, init=0.1, tol=0.01)$rot, 3.872, tolerance = .01,
                info = "Fel: Approximationen är felaktig för x = 15, init = 1.5 och tol = 0.01.")
   expect_equal(babylon(x=15, init=0.1, tol=0.01)$iter, 9,
                info = "Fel: Antalet iterationer är felaktig för x = 15, init = 1.5 och tol = 0.01.")  

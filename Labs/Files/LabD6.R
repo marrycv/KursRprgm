1ae0f53e95b8a51646fccf8fd0fac4dd1aaaddc9
Måns Magnusson <mons.magnusson@gmail.com>
2014-02-24 14:18:47 +0100

    Updated

--- Labs/Files/LabD6.R
+++ Labs/Files/LabD6.R
@@ -1,8 +1,17 @@
 #----------------------------------------------------
 #----------------------------------------------------
-# labb 6 --------------------------------------------
+#### Programmering i R ####
+#### Inlämning, Datalaboration 5 ####
 #----------------------------------------------------
 #----------------------------------------------------
+
+# Ange gruppnummer här
+GruppNr <- "Mitt gruppnummer (ex. 4)"
+# Ange namnen här för alla i gruppen, ex Namn <-c( "Alice Walker","Gustav Vasa")
+Namn <- c("ex. Måns Magnusson", "ex. Josef Wilzén")
+# Ange Liu-ID här för alla i gruppen, ex LiuId <- c("aliwa432","gusva204")
+LiuId  <- c("ex. manma97", "ex. joswi128")
+
 
 
 #----------------------------------------------------

6aa98d056e7ecf017f5dd21c2b357e06db6e3d23
josefwilzen <josefwilzen@gmail.com>
2014-02-27 11:33:23 +0100

    demo

---
+++ Demonstrations/demo_13.R
@@ -0,0 +1,37 @@
+#### Demo 11: Föreläsning 7 ####
+#### Författare: Josef Wilzén
+# test, strinr och linjär algebra
+
+
+# testa regular expressions:
+?regexp
+set
+
+
+grep(pattern="staden",poem)
+
+grep(pattern="han",poem)
+
+grep(pattern="v.r",poem)
+
+grep(pattern="^[a-ö]",poem)
+
+grep(pattern="\\.$",poem)
+
+grep(pattern="\\.+",poem)
+
+grep(pattern="\\.{2,}$",poem)
+
+grep(pattern="vara?",poem)
+
+grep(pattern="tt|ll",poem)
+
+grep(pattern="a(tt|ll)a?",poem) 
+
+
+
+
+
+
+
+

251c89c735c3f2039aa8d3394c2a8f7657fc3602
Måns Magnusson <mons.magnusson@gmail.com>
2014-03-10 10:22:45 +0100

    Added Roxygen type documentation

--- Labs/Files/LabD8_function.R
+++ Labs/Files/LabD8_function.R
@@ -13,21 +13,33 @@ Namn <- c("ex. Måns Magnusson", "ex. Josef Wilzen")
 # Ange Liu-ID här f?r alla i gruppen, ex LiuId <- c("aliwa432","gusva204")
 LiuId  <- c("ex. manma97", "ex. joswi128")
 
+
+
+# Funktioner ####
 # Här nedan ska ni skriva de funktioner som ni skapar själva
-# Det är viktigt att ni kommenterar era funktioner, följ då exemplet nedan, 
-# Funktionen nedan är ett exempel, som inte har nått med uppgifterna att göra.
+# Det är viktigt att ni dokumenterar era funktioner korrekt, 
+# nedan finns ett exempel på detta.
+# Detta sätt att dokumentera bygger på standarden ROxygen2, det är 
+# ett paket för att automatiskt generera R-hjälpfiler från kod.
+# Mer information finns här:
+# http://adv-r.had.co.nz/Documenting-functions.html
+
+# Funktionen nedan är ett exempel, som inte har något med uppgifterna att göra.
 
 
+#' @title mySin
+#'
+#' @description 
+#' Beräknar och plottar olika sinuskurvor 
+#' 
+#' @param x numerisk vektor
+#' @param amp amplituden i sinus
+#' @param phase fasen i sinus
+#' 
+#' @return 
+#' Funktionen returnerar inget värde, den skapar bara en plot.
+#' 
 
-#----------------------------------------------------
-# Funktion: mySin
-#----------------------------------------------------
-# Syfte: Beräkna och plotta olika sinuskurvor
-# Argument
-# x: numerisk vektor
-# amp: amplituden i sinus
-# phase: fasen i sinus
-#----------------------------------------------------
 mySin<-function(x,amp,phase){
   y<-amp*sin(phase*x)  # beräkna sinuskurva
   plot(x,y,type="l")  # plotta resultatet

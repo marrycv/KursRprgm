909996bdef2bb68cfbb96216af4bcb539fa656b1
josefwilzen <josefwilzen@gmail.com>
2014-03-10 09:12:03 +0100

    updates

--- Demonstrations/demo_15.R
+++ Demonstrations/demo_15.R
@@ -1,20 +1,38 @@
-#### Demo 15: FÃ¶relÃ¤sning 8 ####
+#### Demo 15: Föreläsning 8 ####
 
-# linjÃ¤r regression
+# linjär regression
 
 data(trees)
 x<-lm(formula=Volume~Height,data=trees)
 plot(trees$Height,trees$Volume,pch=15)
-y<-coef(x)
-abline(coef=y)
 abline(reg=x)
-plot(y)
+
 plot(x)
-?plot
-example(plot)
-trees
-cars
-<-formula(x=x)
+summary(x)
+coef(x)
+fitted(x)
+
+# kolla under value:
+?lm
+class(x)
+attributes(x)
+str(x)
+# välja ut olika delar av resultatet
+x$coefficients
+x$model
+x$fitted.values
+
+
+y<-summary(x)
+?summary.lm()
+class(y)
+str(y)
+# Koef. och p-värdet:
+y$coefficients
+# den skattade standardavvikelsen på modellen:
+y$sigma
+
+# skapa ett formel-objekt 
 y<-rnorm(10)
 x1<-1:10
 x2<-20:10
@@ -27,5 +45,59 @@ plot.formula(z)
 as.formula("y~.")
 as.formula("y~x1:x2")
 ?"~"
+
+
+# model med flera X-variabler.
+setwd("/home/joswi05/Dropbox/Rkurs/KursRprgm/Labs/DataFiles")
+dir()
+# läsa in data:
+google<-read.csv2(file="GoogleLabb5.csv")
+names(google)
+
+a<-lm(formula=Close~.,data=google)
+b<-summary(a)
+
+
+
+# fiktiv tidseriedata:
+
+x<-1:120
+set.seed(12422)
+monthDiff<-rnorm(12,sd=10)
+y<-10+0.46*x+monthDiff+rnorm(120,sd=3)
+
+plot(x,y,type="l")
+summary
+
+z<-y-monthDiff
+plot(x,z,type="l")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

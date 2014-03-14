8ce2a6bdec2f8baa67e4725e21ec575b836c8ae8
josefwilzen <josefwilzen@gmail.com>
2014-03-14 12:42:20 +0100

    updates

--- Labs/Test/subPlotFuncTest.R
+++ Labs/Test/subPlotFuncTest.R
@@ -3,7 +3,7 @@
 # library(testthat)
 # source("/home/joswi05/Dropbox/Josef/732G33 josef/labbar/från  labbass/test_student.R")
 
-test_that("Assignment: myStatsFunc()", {
+test_that("Assignment: subPlotFunc()", {
   
 
   
@@ -18,52 +18,56 @@ test_that("Assignment: myStatsFunc()", {
   
   
   # ladda ner fil:
-  testApple <- tempfile(pattern="TestApple", fileext=".csv")
-  download.file(url="https://raw.github.com/MansMeg/KursRprgm/master/Labs/DataFiles/AppleLabb5.csv", 
-                destfile=testApple, method="curl", quiet=TRUE)
-  apple<-read.csv2(file=testApple) # läsa in  
+#   testApple <- tempfile(pattern="TestApple", fileext=".csv")
+#   download.file(url="https://raw.github.com/MansMeg/KursRprgm/master/Labs/DataFiles/AppleLabb5.csv", 
+#                 destfile=testApple, method="curl", quiet=TRUE)
+#   apple<-read.csv2(file=testApple) # läsa in  
   
   # övergripande om funktionen:
-  expect_that(myStatsFunc, is_a("function"),
-              info = "Fel: myStatsFunc är inte en funktion.")
-  expect_that(all(names(formals(myStatsFunc)) %in% c("myDataSet","numericFunc","categoricFunc")), condition=is_true(),
+  expect_that(subPlotFunc, is_a("function"),
+              info = "Fel: subPlotFunc är inte en funktion.")
+  expect_that(all(names(formals(subPlotFunc)) %in% c("myData","rowLength","colLength","myOrder")), condition=is_true(),
               info = "Fel: Namn på argumenten i funktionen är fel.")
-  expect_that(myStatsFunc(myDataSet=test4,numericFunc=mean,categoricFunc=sort), is_a("list"),
-              info = "Fel: Funktionen returnerar inte en lista.")
+
+
+  load(file="/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/SolutionData/testDataLab5Plot.Rdata")   
+  source("/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/Solutions/testImageAsMatrix.R")
   
-  # testa de 5 fallen:
-  # workning dic i temp.mappen
-  #   setwd(tempdir())
-  #   statFuncData <- tempfile(pattern="statFuncData", fileext=".Rdata")
-  #   download.file(url="https://github.com/MansMeg/KursRprgmTeacher/tree/master/Labs/SolutionData/testDataLab5.Rdata", 
-  #              destfile=statFuncData, method="curl", quiet=TRUE)
-  #   load(file=statFuncData)
-  #   readLines(con=statFuncData)
-  # Läs in tesdata:
-  load(file="/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/SolutionData/testDataLab5.Rdata")   
+  if(exists(x="sinkToDir")){
+    print("hittar sinkToDir")
+  } else{
+    print("hittar inte sinkToDir")
+  }
   
+
+#   reltativ skillnad i expect_equal()  
+#   tolerance = 0.002,
+#   scale = expectedValue
   
-  # 1: inga variabler
-  expect_equal(myStatsFunc(myDataSet=optionsTest1$dataSet,
-                           numericFunc=optionsTest1$numericFunc,categoricFunc=optionsTest1$categoricFunc),testOutput1,
-               info = "Klarar inte av fallet med varken numeric eller character variabler.")
-  # 2: bara numeriska variabler
-  expect_equal(myStatsFunc(myDataSet=optionsTest2$dataSet,
-                           numericFunc=optionsTest2$numericFunc,categoricFunc=optionsTest2$categoricFunc),testOutput2,
-               info = "Klarar inte av fallet med bara numeriska variabler")
-  # 3: bara character variabler
-  expect_equal(myStatsFunc(myDataSet=optionsTest3$dataSet,
-                           numericFunc=optionsTest3$numericFunc,categoricFunc=optionsTest3$categoricFunc),testOutput3,
-               info = "Klarar inte av fallet med bara character/factor variabler")
-  # 4: Både numeric och character
-  expect_equal(myStatsFunc(myDataSet=optionsTest4$dataSet,
-                           numericFunc=optionsTest4$numericFunc,categoricFunc=optionsTest4$categoricFunc),testOutput4,
-               info = "Klarar inte av fallet med flera numeric variabler och flera character/factor variabler")
-  
-  # 5: Bara en numeric och och en character
-  expect_equal(myStatsFunc(myDataSet=optionsTest5$dataSet,
-                           numericFunc=optionsTest5$numericFunc,categoricFunc=optionsTest5$categoricFunc),testOutput5,
-               info = "Klarar inte av fallet med en numeric variabel och en character/factor variabel") 
+  # 1:  3 rader och 1 kolumner
+  studentPlot1<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions1$myData,rowLength=plotOptions1$rowLength,
+                                   colLength=plotOptions1$colLength,myOrder=plotOptions1$myOrder)
+  expect_equal(studentPlot1,testPlot1,
+             info = "Klarar inte test 1 (se labbinstruktionen).")
+
+  # 2:  testar att med icke konsistenta argument
+  test2String<-"Argumenten funkar ej tillsammans"
+  #prints_text(regexp=test2String,subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8))
+  expect_output(subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8),regexp=test2String,
+                info="Klarar inte test 2 (se labbinstruktionen). Rätt text skrivs inte ut till konsolen.")
+ 
+  # 3:  1 rader och 3 kolumner
+  studentPlot3<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions3$myData,rowLength=plotOptions3$rowLength,
+                                   colLength=plotOptions3$colLength,myOrder=plotOptions3$myOrder)
+  expect_equal(studentPlot3,testPlot3,
+             info = "Klarar inte test 3 (se labbinstruktionen).")
+  # 4:  2 rader och 3 kolumner
+  studentPlot4<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions4$myData,rowLength=plotOptions4$rowLength,
+                                   colLength=plotOptions4$colLength,myOrder=plotOptions4$myOrder)
+  expect_equal(studentPlot3,testPlot3,
+             info = "Klarar inte test 4 (se labbinstruktionen).")
+
+
   setwd(oldWD)
 })
 

14e1809838f0aa792bd2662ab7135843851bf3cd
josefwilzen <josefwilzen@gmail.com>
2014-03-14 14:08:36 +0100

    ny testning av fel på bilder

--- Labs/Test/subPlotFuncTest.R
+++ Labs/Test/subPlotFuncTest.R
@@ -28,16 +28,21 @@ test_that("Assignment: subPlotFunc()", {
   # 1:  3 rader och 1 kolumner
   studentPlot1<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions1$myData,rowLength=plotOptions1$rowLength,
                                    colLength=plotOptions1$colLength,myOrder=plotOptions1$myOrder)
-  expect_equal(studentPlot1,testPlot1,
-             info = "Klarar inte test 1 (se labbinstruktionen).")
-
+  resTest1<-compareImages(imageOrginal=testPlot1,imageStudent=studentPlot1)
+  round(resTest1$relativeError*100,3)
+  textError<-paste("Klarar inte test 1 (se labbinstruktionen). Bilden är ",round(resTest1$relativeError*100,3),"% korrekt.")
+  expect_true(object=resTest1$relativeError>0.999,info="textError")
+  
+#   expect_equal(studentPlot1,testPlot1,
+#              info = "Klarar inte test 1 (se labbinstruktionen).")
+#   image(abs(studentPlot1-testPlot1))
 
   # 2:  testar att med icke konsistenta argument
   test2String<-"Argumenten funkar ej tillsammans"
   #prints_text(regexp=test2String,subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8))
   expect_output(subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8),regexp=test2String,
                 info="Klarar inte test 2 (se labbinstruktionen). Rätt text skrivs inte ut till konsolen.")
- 
+
 
   # 3:  1 rader och 3 kolumner
   studentPlot3<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions3$myData,rowLength=plotOptions3$rowLength,

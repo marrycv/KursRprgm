58e7452abea7319e69264c977968316487cec807
josefwilzen <josefwilzen@gmail.com>
2014-03-14 13:22:10 +0100

    cleaning the code

--- Labs/Test/subPlotFuncTest.R
+++ Labs/Test/subPlotFuncTest.R
@@ -6,26 +6,9 @@
 test_that("Assignment: subPlotFunc()", {
   
 
-  
-  oldWD<-getwd()
-  # workning dic i temp.mappen
-  setwd(tempdir())
-  
-  #testFile <- tempfile(pattern="TestFile", fileext=".csv")
-  
-  # download.file(url="https://raw.github.com/MansMeg/KursRprgm/master/Demonstrations/Apple.csv", 
-  #             destfile=testFile, method="curl", quiet=TRUE)
-  
-  
-  # ladda ner fil:
-#   testApple <- tempfile(pattern="TestApple", fileext=".csv")
-#   download.file(url="https://raw.github.com/MansMeg/KursRprgm/master/Labs/DataFiles/AppleLabb5.csv", 
-#                 destfile=testApple, method="curl", quiet=TRUE)
-#   apple<-read.csv2(file=testApple) # läsa in  
-  
-  # övergripande om funktionen:
   expect_that(subPlotFunc, is_a("function"),
               info = "Fel: subPlotFunc är inte en funktion.")
+  
   expect_that(all(names(formals(subPlotFunc)) %in% c("myData","rowLength","colLength","myOrder")), condition=is_true(),
               info = "Fel: Namn på argumenten i funktionen är fel.")
 
@@ -33,13 +16,12 @@ test_that("Assignment: subPlotFunc()", {
   load(file="/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/SolutionData/testDataLab5Plot.Rdata")   
   source("/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/Solutions/testImageAsMatrix.R")
   
-  if(exists(x="sinkToDir")){
+  if(exists(x="sinkToDir",inherits=TRUE)){
     print("hittar sinkToDir")
   } else{
     print("hittar inte sinkToDir")
   }
   
-
 #   reltativ skillnad i expect_equal()  
 #   tolerance = 0.002,
 #   scale = expectedValue
@@ -50,25 +32,26 @@ test_that("Assignment: subPlotFunc()", {
   expect_equal(studentPlot1,testPlot1,
              info = "Klarar inte test 1 (se labbinstruktionen).")
 
+
   # 2:  testar att med icke konsistenta argument
   test2String<-"Argumenten funkar ej tillsammans"
   #prints_text(regexp=test2String,subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8))
   expect_output(subPlotFunc(myData=plotOptions1$myData,rowLength=3,colLength=1,myOrder=1:8),regexp=test2String,
                 info="Klarar inte test 2 (se labbinstruktionen). Rätt text skrivs inte ut till konsolen.")
  
+
   # 3:  1 rader och 3 kolumner
   studentPlot3<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions3$myData,rowLength=plotOptions3$rowLength,
                                    colLength=plotOptions3$colLength,myOrder=plotOptions3$myOrder)
   expect_equal(studentPlot3,testPlot3,
              info = "Klarar inte test 3 (se labbinstruktionen).")
+
   # 4:  2 rader och 3 kolumner
   studentPlot4<-createMatrixFromPlot(plotCode=subPlotFunc,myData=plotOptions4$myData,rowLength=plotOptions4$rowLength,
                                    colLength=plotOptions4$colLength,myOrder=plotOptions4$myOrder)
   expect_equal(studentPlot3,testPlot3,
              info = "Klarar inte test 4 (se labbinstruktionen).")
 
-
-  setwd(oldWD)
   }
 )
 

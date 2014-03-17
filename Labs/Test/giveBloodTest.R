567e39d3f32ef836eb0d78e6fb24cba2108b3244
josefwilzen <josefwilzen@gmail.com>
2014-03-17 17:32:08 +0100

    updates

--- Labs/Test/giveBloodTest.R
+++ Labs/Test/giveBloodTest.R
@@ -1,8 +1,8 @@
 ### Assignment : sumOfRandomDice() ###
-rm(list=ls())
-library(testthat)
-source("/home/joswi05/Dropbox/RCourse2014/Admin/StudentSolutions/D6/emmajosef6_grupp14.R")
-source("/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/Solutions/LabSolutions_6.R")
+# rm(list=ls())
+# library(testthat)
+# source("/home/joswi05/Dropbox/RCourse2014/Admin/StudentSolutions/D6/emmajosef6_grupp14.R")
+# source("/home/joswi05/Dropbox/Rkurs/KursRprgmTeacher/Labs/Solutions/LabSolutions_6.R")
 
 test_that("Assignment: sumOfRandomDice()", {
   
@@ -25,21 +25,16 @@ test_that("Assignment: sumOfRandomDice()", {
   expect_equal(do.call(giveBlood,bloodOptions1Female),resBlood1Female,
                info = errorInfo(parameters=bloodOptions1Female,funcName="giveBlood"))
   
-  
   # testfall 2:
   expect_equal(do.call(giveBlood,bloodOptions2Male),resBlood2Male,
                info = errorInfo(parameters=bloodOptions2Male,funcName="giveBlood"))
   expect_equal(do.call(giveBlood,bloodOptions2Female),resBlood2Female,
                info = errorInfo(parameters=bloodOptions2Female,funcName="giveBlood"))
   
-  
-  
   # testfall 4:
   expect_equal(do.call(giveBlood,bloodOptions3Male),resBlood3Male,
                info = errorInfo(parameters=bloodOptions3Male,funcName="giveBlood"))
   expect_equal(do.call(giveBlood,bloodOptions3Female),resBlood3Female,
                info = errorInfo(parameters=bloodOptions3Female,funcName="giveBlood"))
-  
-  
-  
+    
 })

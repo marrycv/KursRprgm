0b7cdb7cf1a689c8fbaeff2fbba70acb6749cb73
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-09 21:49:22 +0100

    Changed encoding to UTF8

--- Labs/Tests/Tasks/logical_equality_tests.R
+++ Labs/Tests/Tasks/logical_equality_tests.R
@@ -4,11 +4,11 @@ context("logical_equality()")
 
 test_that("Kontroll av logical_equality.", {
   expect_that(logical_equality, is_a("function"),
-              info = "Fel: logical_equality ‰r inte en funktion.")
+              info = "Fel: logical_equality √§r inte en funktion.")
   expect_that(all(names(formals(logical_equality)) %in% c("A", "B")), condition=is_true(),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
   expect_that(is.logical(logical_equality(TRUE, TRUE)), condition=is_true(),
-              info = "Fel: Funktionen returnerar inte ett logiskt v‰rde.")
+              info = "Fel: Funktionen returnerar inte ett logiskt v√§rde.")
   expect_that(logical_equality(TRUE, TRUE), is_true(),
               info = "Fel: logical_equality(TRUE, TRUE) returnerar inte TRUE.")
   expect_that(!logical_equality(FALSE, TRUE), is_true(),

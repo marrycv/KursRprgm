2bf8e07ae57295d7a9e7b9855ec8a7d5c14fb95a
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-09 21:26:34 +0100

    Update mandatory

--- Labs/Tests/mandatory.R
+++ Labs/Tests/mandatory.R
@@ -1,12 +1,12 @@
 ### Assignment structure ###
 
-context("Labbinformation: ")
+context("Labbinformation")
 
 test_that("Obligatoriska object", {
   expect_true(exists("Namn"), info = "Variabeln 'Namn' saknas.")
   expect_true(exists("LiuId"), info = "Variabeln 'LiuId' saknas.")
-  expect_is(Namn, "character", info = "Variabeln 'Namn' är inte en textvariabel.")
-  expect_is(LiuId, "character", info = "Variabeln 'LiuId' är inte en textvariabel.")
+  expect_is(Namn, "character", info = "Variabeln 'Namn' ska vara en textvariabel.")
+  expect_is(LiuId, "character", info = "Variabeln 'LiuId' ska vara en textvariabel.")
 })
 
 library(testthat)

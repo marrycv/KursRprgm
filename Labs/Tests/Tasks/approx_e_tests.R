48d86be8be80237627ae3ddf884b5f793938b504
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-15 19:34:11 +0100

    Added free variable tests

--- Labs/Tests/Tasks/approx_e_tests.R
+++ Labs/Tests/Tasks/approx_e_tests.R
@@ -7,6 +7,8 @@ test_that("Kontroll av approx_e.", {
               info = "Fel: approx_e() saknas.")
   expect_that(approx_e, is_a("function"),
               info = "Fel: approx_e är inte en funktion.")
+  expect_self_contained(object = approx_e,
+                        "Fel: Funktionen innehåller fria variabler")
   expect_that(all(names(formals(approx_e)) %in% c("N")), condition=is_true(),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
   expect_equal(is.numeric(approx_e(2)), TRUE,

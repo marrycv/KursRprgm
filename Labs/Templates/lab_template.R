6fc31a0bfb951241271ea18eb03a1c6dca39e63f
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-15 15:11:15 +0100

    Add templates

---
+++ Labs/Templates/lab_template.R
@@ -0,0 +1,20 @@
+#### Programmering i R ####
+#### Inlämning, Datalaboration 1 ####
+
+# Ange ditt namn här, ex Namn <- "Alice Walker"
+Namn <- ""
+# Ange ditt LiU-ID här, ex LiuId <- "aliwa123"
+LiuId  <- ""
+
+
+# Uppgift 1 ----
+uppgift1 <- function(mitt_argument){
+  # Skriv din funktion här
+}
+
+
+# Använda markmyassignment
+# library(markmyassignment)
+# set_assignment("[ANGE LABBUPPGIFTENS SÖKVÄG HÄR (FINNS I LABBEN)]")
+# show_tasks()
+# mark_my_assignment()

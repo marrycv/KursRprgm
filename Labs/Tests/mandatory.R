bb164448d843c8783ed9999dc1a732492b32528a
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-16 08:45:10 +0100

    Updated mandatory tests

--- Labs/Tests/mandatory.R
+++ Labs/Tests/mandatory.R
@@ -6,7 +6,9 @@ test_that("Obligatoriska object", {
   expect_true(exists("Namn"), info = "Variabeln 'Namn' saknas.")
   expect_true(exists("LiuId"), info = "Variabeln 'LiuId' saknas.")
   expect_is(Namn, "character", info = "Variabeln 'Namn' ska vara en textvariabel.")
+  expect_true(nchar(Namn) > 0, info = "Ange ditt Namn.")
   expect_is(LiuId, "character", info = "Variabeln 'LiuId' ska vara en textvariabel.")
+  expect_true(nchar(LiuId) > 0, info = "Ange ditt LiuId.")
 })
 
 library(testthat)

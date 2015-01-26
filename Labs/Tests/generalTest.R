6788364a9c8c9105f19001dd84699e185753797d
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-26 20:51:58 +0100

    Update

--- Labs/Tests/generalTest.R
+++
@@ -1,9 +0,0 @@
-### Assignment : generalTests ###
-
-test_that("Kontroll av inlämningsfil.", {
-  cat("File : ")
-  expect_that(exists("Namn"), is_true(),
-              info = "'Namn' saknas i filen.")
-  expect_that(exists("LiuId"), is_true(),
-              info = "'LiuId' saknas i filen.")
-})

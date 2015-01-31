ba5dd30a0cb139379c378cde24ab6b8280b355c7
Måns Magnusson <mons.magnusson@gmail.com>
2015-01-31 21:39:06 +0100

    Update

--- Labs/Tests/Tasks/dynamic_linear_system_tests.R
+++ Labs/Tests/Tasks/dynamic_linear_system_tests.R
@@ -23,14 +23,14 @@ test_that("Assignment: dynamic_linear_system", {
               c("n", "var1", "var2", "var3")), is_true(),
               info="Fel: Funktionen returnerar inte korrekta variabelnamn (om f_0 saknar namn).")
   expect_equal(round(dynamic_linear_system(f_0, A, n)[,2],2), c(20,49.69), 
-            info="Fel: Funktionen returnerar felaktiga beräkningar")
+            info="Fel: Funktionen returnerar felaktiga berakningar")
   expect_equal(round(dynamic_linear_system(f_0, A, n)[,4],2), c(40,30.52), 
-               info="Fel: Funktionen returnerar felaktiga beräkningar")
+               info="Fel: Funktionen returnerar felaktiga berakningar")
 
   names(f_0) <- c("Fido","Telus","Rogers")
   expect_that(all(names(dynamic_linear_system(f_0, A, n)) %in% 
                     c("n","Fido","Telus","Rogers")), is_true(),
-              info="Fel: Funktionen returnerar inte korrekta variabelnamn (om f_0 är namngiven).")
+              info="Fel: Funktionen returnerar inte korrekta variabelnamn (om f_0 ar namngiven).")
 
   A <- matrix(c(.3, .2, -.3, 1.1),ncol=2,byrow = TRUE) 
   f_0 <- c(5,1)
@@ -40,9 +40,8 @@ test_that("Assignment: dynamic_linear_system", {
                     c("n", "var1", "var2")), is_true(),
               info="Fel: Funktionen returnerar inte korrekta variabelnamn (om f_0 saknar namn).")
   expect_equal(round(dynamic_linear_system(f_0, A, n)[,2],2), c(5.00,-0.33,-0.41), 
-               info="Fel: Funktionen returnerar felaktiga beräkningar")
+               info="Fel: Funktionen returnerar felaktiga berakningar")
   expect_equal(round(dynamic_linear_system(f_0, A, n)[,3],2), c(1.00,-1.32,-1.46), 
-               info="Fel: Funktionen returnerar felaktiga beräkningar")
+               info="Fel: Funktionen returnerar felaktiga berakningar")
   
-
 })

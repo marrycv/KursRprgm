e2a6e4421f17c34c73f837564d8e0f8ce74bc1ca
Måns Magnusson <mons.magnusson@gmail.com>
2015-02-16 16:41:07 +0100

    Corrections of my_two_by_two_det

--- Labs/Tests/Tasks/two_by_two_det_tests.R
+++ Labs/Tests/Tasks/two_by_two_det_tests.R
@@ -10,9 +10,9 @@ test_that("two_by_two_det()", {
                         "Fel: Funktionen innehåller fria variabler")
   expect_that(all(names(formals(two_by_two_det)) %in% c("x")), condition=is_true(),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
-  expect_equal(two_by_two_det(matrix(1:4, ncol=2)),c(-5),
+  expect_equal(two_by_two_det(matrix(1:4, ncol=2)),c(-2),
                info="Fel: Funktionen returnerar fel determinant för matrix(1:4, ncol=2)")
-  expect_equal(two_by_two_det(matrix(9:12, ncol=2)),c(-13),
+  expect_equal(two_by_two_det(matrix(9:12, ncol=2)),c(-2),
                info="Fel: Funktionen returnerar fel determinant för two_by_two_det(matrix(9:12, ncol=2)")
   expect_error(two_by_two_det(matrix(1:9, ncol=3)),
                info="Fel: Funktionen avbryter/stoppar inte 3*3 matriser")

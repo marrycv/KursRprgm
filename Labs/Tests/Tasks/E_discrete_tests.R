bbc8f5f43692a5e1c5d136896728a4d626a41118
josefwilzen <josefwilzen@gmail.com>
2016-02-08 10:12:46 +0100

    tests for E_discrete

---
+++ Labs/Tests/Tasks/E_discrete_tests.R
@@ -0,0 +1,67 @@
+context("E_discrete()")
+
+test_that("E_discrete()", {
+  x<-1:10
+  px<-rep(1/10,10)
+  x_mat<-cbind(x,px)
+  
+  y<-1:3
+  py<-c(0.2,0.7,0.1)
+  y_mat<-cbind(y,py)
+  
+  expect_that(exists("E_discrete"), is_true(),
+              info = "Fel: E_discrete() saknas.")
+  expect_that(E_discrete, is_a("function"),
+              info = "Fel: E_discrete är inte en funktion.")
+  expect_self_contained(object = E_discrete,
+                        "Fel: Funktionen innehåller fria variabler")
+  expect_that(all(names(formals(E_discrete)) %in% c("density_matrix", "trans")), condition=is_true(),
+              info = "Fel: Argumenten i funktionen har felaktiga namn.")
+  expect_function_code(object = E_discrete, expected = "return", 
+                       info = "Fel: return() saknas i funktionen.")
+  
+  expect_error(E_discrete(density_matrix = matrix("hej")),
+               info = "Fel: Funktionenen stoppar inte när density_matrix är icke numerisk.")  
+  
+  expect_error(E_discrete(density_matrix = matrix("hej")), "density_matrix is not numeric!",
+               info = "Fel: Funktionen returnerar inte rätt felmeddelande då density_matrix är icke numerisk.")  
+ 
+  
+  expect_error(E_discrete(density_matrix = matrix("hej")),
+               info = "Fel: Funktionenen stoppar inte när trans är icke numerisk.")  
+  
+  expect_error(E_discrete(trans = matrix("hej")), "trans is not numeric!",
+               info = "Fel: Funktionen returnerar inte rätt felmeddelande då trans är icke numerisk.")  
+  
+  
+  expect_error(E_discrete(density_matrix = cbind(y,c(0.4,0.6,0.8))),
+               info = "Fel: Funktionenen stoppar inte när sannolikheterna inte summerar till 1.")  
+  
+  expect_error(E_discrete(density_matrix = cbind(y,c(0.4,0.6,0.8))), "probs does not sum to 1!",
+               info = "Fel: Funktionen returnerar inte rätt felmeddelande när sannolikheterna inte summerar till 1.") 
+  
+  
+  expect_that(class(E_discrete()), 
+              is_equivalent_to("numeric"),
+              info="Fel: Funktionen returnerar inte en numerisk vector")
+  
+  expect_equal(length(E_discrete(density_matrix = x_mat)), c(1),
+               info="Fel: Funktionen returnerar en vektor av lengd 1")
+  
+  expect_equal(E_discrete(density_matrix = x_mat),c(5.5),
+               info="Fel: funktionen returnerar inte rätt värde med density_matrix = x_mat")
+  
+  expect_equal(E_discrete(density_matrix = y_mat),c(1.9),
+               info="Fel: funktionen returnerar inte rätt värde med density_matrix = y_mat")
+  
+  expect_equal(E_discrete(density_matrix = y_mat,trans = c(4,0)),c(7.6),
+               info="Fel: funktionen returnerar inte rätt värde med density_matrix = y_mat,trans = c(4,0)t")
+  
+  expect_equal(E_discrete(density_matrix = y_mat,trans = c(1,5)),c(6.9),
+               info="Fel: funktionen returnerar inte rätt värde med density_matrix = y_mat,trans = c(1,5)")
+  
+  expect_equal(E_discrete(density_matrix = y_mat,trans = c(0,5)),c(5),
+               info="Fel: funktionen returnerar inte rätt värde med density_matrix = y_mat,trans = c(0,5)")
+
+  
+})

e1fc1bfa9a100e24ae611d3ae1dc064d2e396e40
josefwilzen <josefwilzen@gmail.com>
2016-02-24 10:26:04 +0100

    updates

--- Labs/Templates/lab_template.R
+++ Labs/Templates/lab_template.R
@@ -1,6 +1,11 @@
 #### Programmering i R ####
 #### Datorlaboration   ####
 
+# spara den fil enligt:
+# del 1: labb1_josad732.R om ditt liu-ID är josad732 och det är labb 1 .
+# del 2: labb5_grupp01.R om ni tillhör grupp 01 och det är labb 5.
+
+
 # Skiv ditt namn här, ex Namn <- "Alice Walker"
 # På del 2 i kursen, när ni arbetar i par, skriv då namn på formen:  Namn <- "Ada Lovelace och Alice Walker"
 Namn <- ""

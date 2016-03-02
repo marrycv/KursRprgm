e94f189cc77272c69fd2e2e78a8dd370df58135a
josefwilzen <josefwilzen@gmail.com>
2016-03-02 22:20:20 +0100

    updated tests for part 2

---
+++ Labs/Tests/mandatory_part2.R
@@ -0,0 +1,21 @@
+### Assignment structure ###
+
+context("Labbinformation")
+
+test_that("Obligatoriska object", {
+  expect_true(exists("Namn"), info = "Variabeln 'Namn' saknas.")
+  expect_true(exists("LiuId"), info = "Variabeln 'LiuId' saknas.")
+  expect_true(exists("Grupp"), info = "Variabeln 'Grupp' saknas.")
+  
+  expect_is(Namn, "character", info = "Variabeln 'Namn' ska vara en textvariabel.")
+  expect_true(nchar(Namn) > 0, info = "Ange era Namn.")
+  
+  expect_is(LiuId, "character", info = "Variabeln 'LiuId' ska vara en textvariabel.")
+  expect_true(nchar(LiuId) > 0, info = "Ange era LiuId.")
+  
+  expect_is(Grupp, "character", info = "Variabeln 'Grupp' ska vara en textvariabel.")
+  expect_true(nchar(Grupp) > 0, info = "Ange er grupp. Tex grupp01 om ni tillhör grupp 01.")
+  
+})
+
+

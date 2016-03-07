851722eabd6952142d47af5868a70af6d9653e84
josefwilzen <josefwilzen@gmail.com>
2016-03-07 22:33:09 +0100

    updates

--- Labs/Tests/Tasks/wordcount_tests.R
+++ Labs/Tests/Tasks/wordcount_tests.R
@@ -39,7 +39,7 @@ test_that("Assignment: wordcount()", {
             info = "Fel: Variabeln freq ska vara en integervektor.")
   
   expect_equal(suppressMessages(wordcount(text=text1))$word[3:5], c("b", "bb", "c"),
-              info = "ABC")
+              info = "Fel: Funktionen returnerar inte rätt värden på kolumnen word när text=\"a b c aa bb cc a bb cc, a a bb. a cc cc a. b b. c d d a, d c cc d b b a a\"")
   
 #   expect_equal(suppressMessages(wordcount(text=text1))$word[3:5], c("b", "bb", "c"),
 #                info = "Fel: Funktionen returnerar inte rätt värden på kolumnen word när text=\"a b c aa bb cc a bb cc, a a bb. a cc cc a. b b. c d d a, d c cc d b b a a\"")

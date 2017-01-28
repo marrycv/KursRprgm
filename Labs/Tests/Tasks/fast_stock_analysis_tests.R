60764497e893d4e75ac2f0785d19d0ea2da17562
josefwilzen <josefwilzen@gmail.com>
2017-01-28 13:27:35 +0100

    updates

--- Labs/Tests/Tasks/fast_stock_analysis_tests.R
+++ Labs/Tests/Tasks/fast_stock_analysis_tests.R
@@ -5,9 +5,14 @@ test_that("fast_stock_analysis()", {
 
   # Ladda ned data för att testa funktionen på
   testFile <- tempfile(pattern="fast_stock_analysis_test_file", fileext=".csv")
+  testFile2 <- tempfile(pattern="fast_stock_analysis_test_file", fileext=".csv")
   if(!file.exists(testFile)) {
     path <- markmyassignment:::path_type("https://raw.githubusercontent.com/MansMeg/KursRprgm/master/Labs/DataFiles/AppleTest.csv")
     markmyassignment:::get_file.path_http(path = path, dest = testFile)
+  }
+  if(!file.exists(testFile2)) {
+    path <- markmyassignment:::path_type("https://raw.githubusercontent.com/MansMeg/KursRprgm/master/Labs/DataFiles/google2.csv")
+    markmyassignment:::get_file.path_http(path = path, dest = testFile2)
   }
   
   expect_that(exists("fast_stock_analysis"), is_true(),
@@ -28,25 +33,39 @@ test_that("fast_stock_analysis()", {
               info="Fel: Listans element är inte korrekt namngivna")
   expect_function_code(object = fast_stock_analysis, expected = "return", 
                        info = "Fel: return() saknas i funktionen.")  
-  
+  #-----------------------------------------------------------------------------------------------
+  # Testa för AppleTest.csv och period_length=5
   # Create myList1
   myList1 <- fast_stock_analysis(file_path=testFile, period_length=5)
   expect_equal(myList1$total_spridning, 11.82, tolerance = 0.01, 
-              info="Fel: Elementet 'total_spridning' är fel.")
+              info="Fel: Elementet 'total_spridning' är fel, för AppleTest.csv och period_length=5")
   expect_equal(myList1$medel_slutpris, 424.996, tolerance = 0.01, 
-               info="Fel: Elementet 'medel_slutpris' är fel.")
+               info="Fel: Elementet 'medel_slutpris' är fel, för AppleTest.csv och period_length=5")
   expect_equal(myList1$slutpris_upp, FALSE, 
-               info="Fel: Elementet 'slutpris_upp' är fel.")
-  expect_equal(myList1$slutpris_upp, FALSE, 
-               info="Fel: Elementet 'slutpris_upp' är fel.")
+               info="Fel: Elementet 'slutpris_upp' är fel, för AppleTest.csv och period_length=5")
   expect_true(all(myList1$datum %in% c("2012-01-24", "2012-01-18")), 
-              info="Fel: Elementet 'datum' är fel.")
+              info="Fel: Elementet 'datum' är fel, för AppleTest.csv och period_length=5")
   
-  expect_equal(myList1$datum, c("2012-01-24", "2012-01-18"), 
-               info="Fel: Elementet 'datum' är fel.")
+  #expect_equal(myList1$datum, c("2012-01-24", "2012-01-18"), 
+  #             info="Fel: Elementet 'datum' är fel, för AppleTest.csv och period_length=5")
 
 #   expect_equal(myList1, list(total_spridning=11.82,medel_slutpris=424.996,slutpris_upp=FALSE,datum=c("2012-01-24", "2012-01-18")), 
 #                info="Fel: Elementet 'datum' är fel.")
+  
+  #-----------------------------------------------------------------------------------------------
+  # Testa för AppleTest.csv och period_length=5
+  # Create myList2
+  myList2 <- fast_stock_analysis(file_path=testFile, period_length=3)
+  expect_equal(myList2$total_spridning, 8.9, tolerance = 0.01, 
+               info="Fel: Elementet 'total_spridning' är fel, för AppleTest.csv och period_length=3")
+  expect_equal(myList2$medel_slutpris, 422.7067, tolerance = 0.01, 
+               info="Fel: Elementet 'medel_slutpris' är fel, för AppleTest.csv och period_length=3")
+  expect_equal(myList2$slutpris_upp, TRUE, 
+               info="Fel: Elementet 'slutpris_upp' är fel, för AppleTest.csv och period_length=3")
+  expect_true(all(myList2$datum %in% c("2012-01-24", "2012-01-20")), 
+              info="Fel: Elementet 'datum' är fel, för AppleTest.csv och period_length=3")
+  
+  
   
   # Unlink temporary file
   unlink(testFile)

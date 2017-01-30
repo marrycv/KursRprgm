39ae933fe3fccf05833261e29c0af7fb9683b96d
josefwilzen <josefwilzen@gmail.com>
2017-01-30 20:43:32 +0100

    updated tests: removed expect_that()

--- Labs/Tests/Tasks/orth_scalar_prod_tests.R
+++ Labs/Tests/Tasks/orth_scalar_prod_tests.R
@@ -3,15 +3,23 @@
 context("orth_scalar_prod()")
 
 test_that("Kontroll av orth_scalar_prod.", {
-  expect_that(exists("orth_scalar_prod"), is_true(), 
+  
+  expect_true(object = exists("orth_scalar_prod"),
               info = "Fel: orth_scalar_prod() saknas.")
-  expect_that(orth_scalar_prod, is_a("function"),
+  
+  expect_true(is.function(orth_scalar_prod),
               info = "Fel: orth_scalar_prod är inte en funktion.")
+  
   expect_function_self_contained(object = orth_scalar_prod,
                         "Fel: Funktionen innehåller fria variabler")
-  expect_that(all(names(formals(orth_scalar_prod)) %in% c("a", "b")), condition=is_true(),
+  expect_true(object = all(names(formals(orth_scalar_prod)) %in% c("a", "b")),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
-  expect_that(is.numeric(orth_scalar_prod(a=1:3, b=4:6)), condition=is_true(),
+  
+  name_vect<-names(formals(orth_scalar_prod))
+  expect_true(all(name_vect %in% c("a", "b"))&!is.null(name_vect),
+              info = "Fel: Argumenten i funktionen har felaktiga namn.")
+  
+  expect_true(is.numeric(orth_scalar_prod(a=1:3, b=4:6)),
               info = "Fel: Funktionen returnerar inte ett numeriskt värde.")
   expect_equal(length(orth_scalar_prod(a=1:3, b=4:6)), 1,
               info = "Fel: Funktionen returnerar inte ett värde.")

b68e4cef9fc8f96358bdaf903e059c61cd13600d
josefwilzen <you@example.com>
2017-02-01 11:38:14 +0100

    updates

--- Labs/Tests/Tasks/three_elements_tests.R
+++ Labs/Tests/Tasks/three_elements_tests.R
@@ -6,19 +6,19 @@ test_that("Kontroll av three_elements.", {
   expect_true(exists("three_elements"), 
               info = "Fel: three_elements() saknas.")
   expect_true(is.function(three_elements),
-              info = "Fel: three_elements √§r inte en funktion.")
+              info = "Fel: three_elements ‰r inte en funktion.")
   #expect_function_self_contained(object = three_elements,
-  #                               "Fel: Funktionen inneh√•ller fria variabler")
+  #                               "Fel: Funktionen innehÂller fria variabler")
   expect_true(is.numeric(three_elements()),
               info = "Fel: Funktionen returnerar inte en numerisk vektor.")
   expect_equal(length(three_elements()), 3,
-              info = "Fel: Funktionen returnerar inte en vektor av l√§ngd 3.")
+              info = "Fel: Funktionen returnerar inte en vektor av l‰ngd 3.")
   expect_true(log(3) %in% three_elements(), 
-              info="Kommentar: ln(3) saknas / √§r fel")
+              info="Kommentar: ln(3) saknas / ‰r fel")
   expect_true(exp(pi+1) %in% three_elements() | exp(1)^(pi+1) %in% three_elements(), 
-              info="Kommentar: e^(pi+1) saknas / √§r fel")
+              info="Kommentar: e^(pi+1) saknas / ‰r fel")
   expect_true(sin(pi/3) %in% three_elements(), 
-              info="Kommentar: sin(pi/3) saknas / √§r fel")
+              info="Kommentar: sin(pi/3) saknas / ‰r fel")
   
   expect_function_code(object = three_elements, expected = "return\\(", 
                        info = "Fel: return() saknas i funktionen.")  

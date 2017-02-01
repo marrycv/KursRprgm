ee22d277922c270893d11600ba016b4f63070335
josefwilzen <you@example.com>
2017-02-01 14:09:28 +0100

    updates

--- Labs/Tests/Tasks/matrix_trace_tests.R
+++ Labs/Tests/Tasks/matrix_trace_tests.R
@@ -1,33 +1,70 @@
 context("matrix_trace()")
 
 test_that("matrix_trace", {
+  
+  test_object_name<-function(target,true_names=NULL){
+    if(is.function(target)){
+      temp_name<-names(formals(target))
+    }else if(is.list(target)){
+      temp_name<-names(target)
+    }else{
+      stop("target has non valid class!")
+    }
+    
+    if(is.null(true_names)){
+      if(is.null(temp_name)){
+        return(TRUE)
+      }else{
+        return(FALSE)
+      }
+    }else if(is.character(true_names)&length(true_names)>=1){
+      no_names<-length(true_names)
+      no_match<-vector("logical",no_names)
+      if(length(temp_name)!=length(true_names)){
+        return(FALSE)
+      }
+      for(i in 1:no_names){
+        no_match[i]<-any(temp_name%in%true_names[i])
+      }
+      return(all(no_match))
+    }else{
+      stop("true_names has non valid class!") 
+    } 
+  }
+  
+  
   test_matA <- matrix(2*4:7,nrow=2)
   test_matB <- matrix(9:12,nrow=2)
   test_matC <- matrix(7:15 * 3, nrow=3)
   test_matD <- matrix(2:17 * 1.5, nrow=4)
   
-  expect_that(matrix_trace, is_a("function"),
-              info = "Fel: matrixTrace är inte en funktion.")
-  expect_that(all(names(formals(matrix_trace)) %in% c("X")), condition=is_true(),
+  expect_true(exists("matrix_trace"),
+              info = "Fel: matrix_trace() saknas.")
+  expect_true(is.function(matrix_trace),
+              info = "Fel: matrix_trace är inte en funktion.")
+  expect_true(test_object_name(target = matrix_trace,true_names = c("X")),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
   expect_function_self_contained(object = matrix_trace,
                         "Fel: Funktionen innehåller fria variabler")
-  expect_that(class(matrix_trace(test_matA)), is_equivalent_to("numeric"),
+  
+  expect_true(class(matrix_trace(test_matA))=="numeric",
               info="Fel: Funktionen returnerar inte ett numeriskt värde")
-  expect_that(length(matrix_trace(test_matA)), is_equivalent_to(1),
+  
+  expect_true(length(matrix_trace(test_matA))==1,
               info="Fel: Funktionen returnerar inte ett element (numeriskt)")
-  expect_that(matrix_trace(test_matA), 
-              is_equivalent_to(22), 
+  
+  expect_true(matrix_trace(test_matA)==22, 
               info="Fel: Returnerar fel värde för en 2 * 2 matris.")
-  expect_that(matrix_trace(test_matB),
-              is_equivalent_to(21), 
+  
+  expect_true(matrix_trace(test_matB)==21,
               info="Fel: Returnerar fel värde för en 2 * 2 matris.")
-  expect_that(matrix_trace(test_matC), 
-              is_equivalent_to(99), 
+  
+  expect_true(matrix_trace(test_matC)==99, 
               info="Fel: Returnerar fel värde för en 3 * 3 matris.")
-  expect_that(matrix_trace(test_matD), 
-              is_equivalent_to(57), 
+  
+  expect_true(matrix_trace(test_matD)==57, 
               info="Fel: Returnerar fel värde för en 4 * 4 matris.")  
+  
   expect_function_code(object = matrix_trace, expected = "return", 
                        info = "Fel: return() saknas i funktionen.")  
 })

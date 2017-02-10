6d658a0770175dd641c7d932b78b8a1d9900d062
Måns Magnusson <mons.magnusson@gmail.com>
2017-02-10 08:39:24 +0100

    Update presentation and demo code

--- Lectures/F5_demo.R
+++ Lectures/F5_demo.R
@@ -1,6 +1,23 @@
-library(pxweb)
-my_data<-interactive_pxweb()
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+# Demo: PXWEB
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 
+library(pxweb)
+my_data <- interactive_pxweb()
+
+
+# Ladda ned husdjur
+my_data <- 
+  get_pxweb_data(url = "http://api.scb.se/OV0104/v1/doris/sv/ssd/JO/JO0103/HusdjurL",
+                 dims = list(Region = c('00'),
+                             Djurslag = c('70'),
+                             ContentsCode = c('JO0103L1'),
+                             Tid = c('*')),
+                 clean = TRUE)
+
+# Visualisera över tid
 index<-!is.na(my_data$values)
 data2<-my_data[index,]
 temp<-as.character(data2$år)
@@ -9,14 +26,6 @@ my_years<-as.numeric(temp)
 plot(x = my_years,y = data2$values,xlab="år",ylab="hästar",type="o")
 
 
-myDataSetName <- 
-  get_pxweb_data(url = "http://api.scb.se/OV0104/v1/doris/sv/ssd/JO/JO0103/HusdjurL",
-                 dims = list(Region = c('00'),
-                             Djurslag = c('70'),
-                             ContentsCode = c('JO0103L1'),
-                             Tid = c('*')),
-                 clean = TRUE)
-
-
+# Hämta data från ett givet api 
 my_data2<-interactive_pxweb(api = 'api.scb.se', version = 'v1', lang = 'sv')
 

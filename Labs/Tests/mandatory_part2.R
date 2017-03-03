c230255c516c3c0273700a1fdf324d633c32adfc
Måns Magnusson <mons.magnusson@gmail.com>
2017-03-03 11:16:58 +0100

    Small fixes

--- Labs/Tests/mandatory_part2.R
+++ Labs/Tests/mandatory_part2.R
@@ -8,10 +8,10 @@ test_that("Obligatoriska object", {
   expect_true(exists("Grupp"), info = "Variabeln 'Grupp' saknas.")
   
   expect_is(Namn, "character", info = "Variabeln 'Namn' ska vara en textvariabel.")
-  expect_true(nchar(Namn) > 0, info = "Ange era Namn.")
+  expect_true(nchar(Namn[1]) > 0, info = "Ange era Namn.")
   
   expect_is(LiuId, "character", info = "Variabeln 'LiuId' ska vara en textvariabel.")
-  expect_true(nchar(LiuId) > 0, info = "Ange era LiuId.")
+  expect_true(nchar(LiuId[1]) > 0, info = "Ange era LiuId.")
   
   expect_is(Grupp, "character", info = "Variabeln 'Grupp' ska vara en textvariabel.")
   expect_true(nchar(Grupp) > 0, info = "Ange er grupp. Tex grupp01 om ni tillhör grupp 01.")

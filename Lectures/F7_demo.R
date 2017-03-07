cbd3c63b6937ebf658d567d16a94f43fb5cb6642
josefwilzen <you@example.com>
2017-03-07 09:26:56 +0100

    uppdates for 2017

--- Lectures/F7_demo.R
+++ Lectures/F7_demo.R
@@ -1,148 +1,124 @@
 
 
-# demo baserat på artiklen nedan och exempel från dok för stringr:
-# "stringr: modern, consistent string processing" av Hadley Wickham
-# https://journal.r-project.org/archive/2010-2/RJournal_2010-2_Wickham.pdf
-
-
-
-library(stringr)
-
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
-# from doc:
+# ggplot()
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
+library(ggplot2)
+?ggplot()
+
+data(diamonds)
+data(longley)
+?longley
+?qplot
 
 
-# str_c
+# testa ggplot:
 
-str_c("a","b","c")
-paste("a","b","c")
-str_c("a","b","c",sep = "--")
-
-str_c(c("a","b","c"),c("d","e","f"),sep = "--")
-
-
-a<-str_c("a","b","c",sep = "\n")
-a
-cat(a)
-print(a)
-
-# str_length
-my_letters<-str_c(letters,collapse = "")
-str_length(my_letters)
-
-# str_sub
-str_sub(string = my_letters,start = 3,end = 6)
-my_letters
-str_sub(string = my_letters,start = 3,end = 6)<-" hej! "
-my_letters
-
-# str_split
-str_split(string = c("hej, hur är det?"),pattern = "[:space:]")
-str_split(string = c("hej, hur är det?"),pattern = ",")
-
-b<-c("hej, hur är det?","saidjf aslfk w ajfd asdj")
-x<-str_split(string = b,pattern = "[:space:]")
-x[[1]][1]
-
-# str_trim
-
-str_trim(string = c("   hej!", "    abc  ","test   "),side = "left")
-str_trim(string = c("   hej!", "    abc  ","test   "),side = "right")
-str_trim(string = c("   hej!", "    abc  ","test   "),side = "both")
+?ggplot
+# qplot():
+qplot(x=)
+qplot(displ,hwy,data=mpg)
+qplot(displ,hwy,data=mpg, facets=.~drv)
 
 
-# str_pad
-str_pad(string = "HEJ!",width = 10,side ="left")
 
-str_pad(string = "HEJ!",width = 10,side ="both")
+g<-ggplot(mpg,aes(displ,hwy))
 
 
-#----------------------------------------------------------------------------
-#----------------------------------------------------------------------------
+g<-ggplot(mpg,mapping=aes(displ,hwy))
+print(g)
+str(g)
+summary(g)
+class(g)
 
-# pattern matching-funktioner:
-?regex
+p<-g+geom_point()
+print(p)
 
+g+geom_point()+geom_smooth()
+g+geom_point()+geom_smooth(method="lm")
 
-str_detect(string = my_letters,pattern = "ab")
-str_detect(string = my_letters,pattern = "cd")
-str_locate(string = my_letters,pattern = "ab")
+g+geom_point()+geom_smooth(method="lm")
 
+g+geom_point()+facet_grid(.~drv)
 
-x<-" [a-z]{3} "
-x<-"(^| )[a-z]{3} "
-str_locate(string ="hud hej hejhej",pattern = x)
+g+geom_point()+geom_smooth(method="lm")+facet_grid(.~drv)
 
+g+geom_point()+geom_smooth(method="loess")+facet_grid(.~drv)
 
-# str_replace
-
-str_replace(string =" hej hejhej",pattern = x,replacement = "X")
-
-
-# str_extract_all
-
-y<-" hej"
-
-str_extract
-str_extract_all
-
-
-#----------------------------------------------------------------------------
-#----------------------------------------------------------------------------
-
-# str_extract {stringr}
-shopping_list <- c("apples x4", "bag of flour", "bag of sugar", "milk x2")
-str_extract(shopping_list, "\\d")
-str_extract(shopping_list, "[a-z]+")
-str_extract(shopping_list, "[a-z]{1,4}")
-str_extract(shopping_list, "\\b[a-z]{1,4}\\b")
-
-# Extract all matches
-str_extract_all(shopping_list, "[a-z]+")
-str_extract_all(shopping_list, "\\b[a-z]+\\b")
-str_extract_all(shopping_list, "\\d")
-
-# Simplify results into character matrix
-str_extract_all(shopping_list, "\\b[a-z]+\\b", simplify = TRUE)
-str_extract_all(shopping_list, "\\d", simplify = TRUE)
+# För lite data
+g+geom_point()+geom_smooth(method="lm")+facet_grid(fl~drv)
 
 
 
 
-# from article:
+names(mpg)
+?mpg
 
-strings <- c(" 219 733 8965", "329-293-8753 ", "banana", "595 794 7569",
-             "387 287 6718", "apple", "233.398.9187  ", "482 952 3315", "239 923 8115",
-             "842 566 4692", "Work: 579-499-7527", "$1000", "Home: 543.355.3679")
 
-strings
-phone <- "([2-9][0-9]{2})[- .]([0-9]{3})[- .]([0-9]{4})"
-phone
-# Which strings contain phone numbers?
-str_detect(strings, phone)
-strings[str_detect(strings, phone)]
-# Where in the string is the phone number located?
-loc <- str_locate(strings, phone)
-loc
-# Extract just the phone numbers
-str_sub(strings, loc[, "start"], loc[, "end"])
-# Or more conveniently:
-str_extract(strings, phone)
-# Pull out the three components of the match
-str_match(strings, phone)
-# Anonymise the data
-str_replace(strings, phone, "XXX-XXX-XXXX")
+# använda "konstanta" färger:
+g+geom_point(color="steelblue")+geom_smooth(method="lm")
+g+geom_point(color="green",size=4,alpha=0.5)+geom_smooth(method="lm")
 
+
+
+# anv?nda variabla f?rger:
+g+geom_point(aes(color=drv),size=6,alpha=0.5)+geom_smooth(method="lm")
+
+g+geom_point(color="green",size=4,alpha=0.5)+geom_smooth(method="lm")
+
+# ?ndra mer p? plotten...
+g+geom_point(aes(color=drv),size=6,alpha=0.5)+labs(title="mpg data")+ labs(x="x variable is displ")
+
+g+geom_point(aes(color=drv),size=6,alpha=0.5)+labs(title="mpg data")+ labs(x="x variable is displ")
+
+g+geom_point(aes(color=drv),size=6,alpha=0.5)+labs(title="mpg data")+ labs(x="x variable is displ")+
+  geom_smooth(size=2,linetype=5,method="loess", se=FALSE)
+
+
+# tema black and white
+g+geom_point(color="green",size=4,alpha=0.5)+geom_smooth(method="lm") +theme_bw()
+g+geom_point(color="green",size=4,alpha=0.5)+geom_smooth(method="lm") +theme_dark(base_family="")
+
+
+# gr?nser f?r axlarna:
+testData<-data.frame(x=1:100,y=rnorm(100))
+testData[42,2]<-75 # outlier
+plot(testData[,1],testData[,2],type="l",ylim=c(-3,3))
+
+b<-ggplot(testData,aes(x=x,y=y))
+b+geom_line()
+
+b+geom_line()
+# tar bort data som ligger utanf?r gr?nsen
+b+geom_line() +ylim(-3,3)
+# indkluderar data som ligger ?tanf?r gr?nsen
+b+geom_line()+ coord_cartesian(ylim=c(-3,3))
+
+
+
+# longley data:
+
+data(longley)
+d<-ggplot(data=longley,aes(Year,GNP.deflator,Year, GNP))
+e<-ggplot(data=longley,aes(Year, GNP))
+e+geom_line()
+
+
+# ggplot och histgram:
+data(chickwts)
+a<-ggplot(data=chickwts,aes(x=weight))
+a+geom_bar()+facet_grid(.~feed)
+a+geom_histogram(binwidth = 10)
+a+geom_histogram(binwidth = 30)+facet_grid(.~feed)
 
 
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
-
 # regression:
-
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 ?lm
 
 data("trees")

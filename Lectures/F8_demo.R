94dba55be235ba106b06664556c49ce2c3c0d521
Måns Magnusson <mons.magnusson@gmail.com>
2017-03-14 16:04:13 +0100

    Push Lecture 8

--- Lectures/F8_demo.R
+++ Lectures/F8_demo.R
@@ -29,18 +29,20 @@ cat(a)
 print(a)
 
 # str_length
-my_letters<-str_c(letters,collapse = "")
+letters
+my_letters <- str_c(letters, collapse = "")
 str_length(my_letters)
+str_length(letters)
 
 # str_sub
-str_sub(string = my_letters,start = 3,end = 6)
+str_sub(string = my_letters, start = 3, end = 6)
 my_letters
-str_sub(string = my_letters,start = 3,end = 6)<-" hej! "
+str_sub(string = my_letters, start = 3, end = 6) <- " hej! "
 my_letters
 
 # str_split
-str_split(string = c("hej, hur är det?"),pattern = "[:space:]")
-str_split(string = c("hej, hur är det?"),pattern = ",")
+str_split(string = c("hej, hur är det?"), pattern = "[:space:]")
+str_split(string = c("hej, hur är det?"), pattern = ",")
 
 b<-c("hej, hur är det?","saidjf aslfk w ajfd asdj")
 x<-str_split(string = b,pattern = "[:space:]")
@@ -65,20 +67,21 @@ str_pad(string = "HEJ!",width = 10,side ="both")
 # pattern matching-funktioner:
 ?regex
 
+my_letters
+str_detect(string = my_letters, pattern = "ab")
+str_detect(string = my_letters, pattern = "cd")
 
-str_detect(string = my_letters,pattern = "ab")
-str_detect(string = my_letters,pattern = "cd")
-str_locate(string = my_letters,pattern = "ab")
+str_locate(string = my_letters, pattern = "hi")
 
 
-x<-" [a-z]{3} "
-x<-"(^| )[a-z]{3} "
-str_locate(string ="hud hej hejhej",pattern = x)
+x <- "[a-z]{3} "
+x <- "(^| )[a-z]{3} "
+str_locate_all(string ="hud hej hejhej",pattern = x)
 
 
 # str_replace
 
-str_replace(string =" hej hejhej",pattern = x,replacement = "X")
+str_replace(string =" hej hejhej",pattern = x, replacement = "X")
 
 
 # str_extract_all
@@ -134,4 +137,98 @@ str_extract(strings, phone)
 str_match(strings, phone)
 # Anonymise the data
 str_replace(strings, phone, "XXX-XXX-XXXX")
+
+
+
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# Tidy R
+# Taget från: https://blog.rstudio.org/2014/07/22/introducing-tidyr/
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+library(tidyr)
+library(dplyr)
+
+# Multiple observations per row
+messy <- data.frame(
+  name = c("Wilbur", "Petunia", "Gregory"),
+  a = c(67, 80, 64),
+  b = c(56, 90, 50)
+)
+
+tidy <- gather(data = messy, key = drug, value = heartrate, a:b)
+tidy
+
+# Make multiple observations per row again
+spread(tidy, key = drug, value = heartrate)
+
+# Multiple variables in same columns
+set.seed(10)
+messy <- data.frame(
+  id = 1:4,
+  trt = sample(rep(c('control', 'treatment'), each = 2)),
+  work.T1 = runif(4),
+  home.T1 = runif(4),
+  work.T2 = runif(4),
+  home.T2 = runif(4)
+)
+
+tidier <- gather(data = messy, key = key, value = time, -id, -trt)
+# Can use stringr or...
+tidy <- separate(tidier, key, into = c("location", "time"), sep = "\\.") 
+
+
+
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# dplyr
+# Taget från: https://blog.rstudio.org/2014/07/22/introducing-tidyr/
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+install.packages("nycflights13")
+library(dplyr)
+library(nycflights13)
+
+data(flights)
+class(flights)
+
+# Select
+select(.data = flights, day, year, dep_delay)
+
+# Filter
+filter(flights, day == 1 & dep_delay > 60)
+
+# Arrange
+arrange(flights, desc(dep_delay))
+system.time(
+arrange(flights, dep_delay)
+)
+
+system.time(
+flights[order(flights$dep_delay, decreasing = TRUE),]
+)
+
+# Mutate
+flights <- 
+  mutate(flights,
+         gain = arr_delay - dep_delay,
+         speed = distance / air_time * 60)
+select(flights, gain, speed)
+
+# Summarise
+summarise(flights,
+          delay = mean(dep_delay, na.rm = TRUE))
+
+# Group by
+daily <- group_by(flights, year, month, day)
+per_day   <- summarise(daily, flights = n(), delay = mean(dep_delay, na.rm = TRUE))
+
+# Join
+# There are different joins for different purposes, see ?join
+flights2 <- left_join(flights, per_day)
+select(flights2, dep_delay, delay)
 

d2ccd189c1fadedda614932ffed223fbfaa92dc2
Måns Magnusson <mons.magnusson@gmail.com>
2017-12-30 21:24:46 +0100

    Small fix

--- Labs/Tests/Tasks/two_by_two_det_tests.R
+++ Labs/Tests/Tasks/two_by_two_det_tests.R
@@ -11,14 +11,14 @@ test_that("two_by_two_det()", {
   expect_that(all(names(formals(two_by_two_det)) %in% c("x")), condition=is_true(),
               info = "Fel: Argumenten i funktionen har felaktiga namn.")
   expect_error(two_by_two_det("test"),
-               info="Fel: Funktionen avbryter/stoppar om x inte är en matris.")
+               info="Fel: Funktionen avbryter/stoppar inte om x inte är en matris.")
   expect_error(two_by_two_det("test"),"Not an object of class matrix.",
-               info="Fel: Funktionen ger inte rätt felmeddeande om x inte är en matris.")
+               info="Fel: Funktionen ger inte rätt felmeddelande om x inte är en matris.")
   
   expect_error(two_by_two_det(matrix(1:9, ncol=3)),
-               info="Fel: Funktionen avbryter/stoppar inte 3*3 matriser")
+               info="Fel: Funktionen avbryter/stoppar inte för 3*3 matriser")
   expect_error(two_by_two_det(matrix(1:9, ncol=3)),"Matrix not of size 2*2.",
-               info="Fel: Funktionen inte rätt felmeddeande för 3*3 matriser")
+               info="Fel: Funktionen returnerar inte rätt felmeddelande för 3*3 matriser")
   
   expect_equal(two_by_two_det(matrix(1:4, ncol=2)),c(-2),
                info="Fel: Funktionen returnerar fel determinant för x=matrix(1:4, ncol=2)")

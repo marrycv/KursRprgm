3138c71cc35ff185252721cb14927ba4bbd33d81
Måns Magnusson <mons.magnusson@gmail.com>
2018-01-17 15:34:16 +0100

    Updated presentation and demo code

---
+++ Lectures/F1_demo.R
@@ -0,0 +1,76 @@
+# Demo-kod: Lecture 1
+
+# variables ----
+
+my_variable <- "Calle"
+my variable <- "Calle"
+
+my_vector <- c("Calle", "Blomqvist")
+my_vector
+
+
+my_num <- 1:10
+my_num
+
+## Variabeltyper
+typeof(my_vector)
+typeof(my_num)
+
+
+# functions ----
+# Funktionens innehåll: 
+# Skriv en funktion som tar ett värde och kvadrerar det:
+# - Börja med kod som fungerar (men med funktionens argument)
+
+# - Lyft in i funktionen
+
+# - Pröva att funktionen fungerar som den ska
+
+
+
+# Lokal miljö
+f <- function(x, y){
+  res <- x+y
+  return(res)
+}
+x <- 5
+y <- 5
+f(2,2)
+
+
+# markmyassignment ----
+## Installation
+install.packages("markmyassignment")
+
+## Använd paketet
+library("markmyassignment")
+
+## Ställ in uppgift
+assignment_path <- 
+  file.path(system.file(package = "markmyassignment"), "extdata", "example_assignment01.yml")
+set_assignment(assignment_path)
+
+
+## Uppgift:
+# Mandatory: Assign you name to my_name 
+my_name <- "Mans Magnusson"
+
+## task1: Create a vector with the values pi and e
+task1 <- c(2 * pi, exp(1)) # Wrong
+task1 <- c(pi, exp(1)) # Correct
+
+## task2: Create a function that return the sum of the first and last element
+task2 <- function(vector){
+  vector[2] + vector[length(vector)] # Wrong
+}
+
+task2 <- function(vector){
+  vector[1] + vector[length(vector)] # Correct
+}
+
+mark_my_assignment()
+
+
+# Logik ----
+
+

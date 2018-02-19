df249723a7a85bacc1ebbef5371c26147f39e0e0
Måns Magnusson <mons.magnusson@gmail.com>
2018-02-19 16:41:37 +0100

    Small updates

--- Lectures/F5_demo.R
+++ Lectures/F5_demo.R
@@ -1,11 +1,64 @@
+data("iris")
+iris
+plot(x = iris$Sepal.Length, y = iris$Petal.Length)
+plot(iris)
+
 # -------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------
-# Demo: PXWEB
+# Demo: Slumptal
 # -------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------
 
+rnorm(n = 5, mean = 10, sd = 1)
+set.seed(20180218)
+rnorm(n = 5, mean = 10, sd = 1)
+set.seed(20180218)
+rnorm(n=5, mean=10, sd=1)
+
+text <- c("Linköpings", "Universitet")
+set.seed(20180218)
+sample(x=text, size=4, replace=TRUE)
+sample(x=text, size=4, replace=TRUE)
+set.seed(20180218)
+sample(x=text, size=4, replace=TRUE)
+
+
+
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+# Demo: Markdown
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+
+# 1. 
+# https://rmarkdown.rstudio.com/
+# https://rmarkdown.rstudio.com/lesson-1.html
+
+# 2. 
+# https://www.rstudio.com/wp-content/uploads/2015/03/rmarkdown-reference.pdf
+
+# 3. 
+# Markdown file + Notebook
+
+
+
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+# Demo: PXWEB + Grafik
+# -------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
+install.packages("pxweb")
 library(pxweb)
 my_data <- interactive_pxweb()
+
+x <- 
+  get_pxweb_data(url = "http://statistik.konj.se/PXWeb/api/v1/sv/KonjBar/hushall/Indikatorhus.px",
+                 dims = list(Indikator = c('*'),
+                             Grupp = c('100', '301', '302', '502'),
+                             Period = c('*')),
+                 clean = TRUE)
+
+
 
 
 # Ladda ned husdjur
@@ -18,14 +71,27 @@ my_data <-
                  clean = TRUE)
 
 # Visualisera över tid
-index<-!is.na(my_data$values)
-data2<-my_data[index,]
-temp<-as.character(data2$år)
-my_years<-as.numeric(temp)
+index <- !is.na(my_data$values)
+data2 <- my_data[index,]
+temp <- as.character(data2$år)
+my_years <- as.numeric(temp)
 
-plot(x = my_years,y = data2$values,xlab="år",ylab="hästar",type="o")
+plot(x = my_years, y = data2$values, xlab="År", ylab="Antal hästar", type="o")
+
+Ni <- rpois(100, lambda = 5)
+tN <- table(Ni)
+barplot(tN)
+
+barplot_data <- data2$values
+names(barplot_data) <- my_years
+barplot(barplot_data)
+
+
+y <- rnorm(1000, mean = 0, sd = 1)
+hist(y)
+hist(y, probability = TRUE)
 
 
 # Hämta data från ett givet api 
-my_data2<-interactive_pxweb(api = 'api.scb.se', version = 'v1', lang = 'sv')
+my_data2 <- interactive_pxweb(api = 'api.scb.se', version = 'v1', lang = 'sv')
 

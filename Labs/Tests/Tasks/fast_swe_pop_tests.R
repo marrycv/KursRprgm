90556d3d66130051adf55dd96a57c66f3ea7a7a8
Måns Magnusson <mons.magnusson@gmail.com>
2018-02-21 12:54:38 +0100

    Small fix

--- Labs/Tests/Tasks/fast_swe_pop_tests.R
+++ Labs/Tests/Tasks/fast_swe_pop_tests.R
@@ -3,25 +3,22 @@ context("fast_swe_pop()")
 
 test_that("fast_swe_pop()", {  
   library(pxweb)
-  expect_that(exists("fast_swe_pop"), is_true(),
+  expect_attached_package("pxweb")
+  expect_true(exists("fast_swe_pop"),
               info = "Fel: fast_swe_pop() saknas.")
-  expect_that(fast_swe_pop, is_a("function"),
-              info = "Fel: fast_swe_pop är inte en funktion.")
-  expect_that(is.null(formals(fast_swe_pop)), condition=is_true(),
-              info = "Fel: Funktionen ska sakna argument.")
-  expect_function_self_contained(object = fast_swe_pop, "Fel: Funktionen innehåller fria variabler")
+  checkmate::expect_function(fast_swe_pop, nargs = 0,
+                             info = "Fel: babylon är inte en funktion.")
+#  expect_function_self_contained(object = fast_swe_pop, "Fel: Funktionen innehåller fria variabler")
+
   
-  res <- fast_swe_pop()
-  expect_true(class(res)=="data.frame", 
-              info="Fel: Funktionen returnerar inte en data.frame.")
-  expect_true(all(names(res) %in% c("year", "population")), 
-               info="Fel: Funktionen returnerar felaktiga variabelnamn.")
-  expect_true(is.numeric(res$year),
-               info="Fel: year ska vara en numerisk variabel.")
-  expect_true(is.numeric(res$population), 
-               info="Fel: population ska vara en numerisk variabel.")
-  expect_true(all(res$year[12:15] == c(1979,1980,1981,1982)),
-               info="Fel: Felaktigt resultat i year.")
-  expect_true(all(res$population[12:15] == c(8303010,8317937,8323033,8327484)),
-               info="Fel: Felaktigt resultat i population.")
+  expect_silent(fast_swe_pop_output <- fast_swe_pop())
+  expect_class(fast_swe_pop_output, "data.frame",
+              info="Fel: fast_swe_pop() returnerar inte en data.frame.")
+  expect_names(names(fast_swe_pop_output), permutation.of =  c("year", "population"))
+  expect_numeric(fast_swe_pop_output$year)
+  expect_numeric(fast_swe_pop_output$population)
+
+  expect_equal(fast_swe_pop_output$year[12:15], c(1979,1980,1981,1982))
+  expect_equal(fast_swe_pop_output$population[12:15], c(8303010,8317937,8323033,8327484))
+
 })

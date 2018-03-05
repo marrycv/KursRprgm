5384ae3d9b679c57c46253f104712df14c8ffedf
Måns Magnusson <mons.magnusson@gmail.com>
2018-03-05 08:54:03 +0100

    Updated tests

--- Labs/Tests/Tasks/diagonalize_matrix_tests.R
+++ Labs/Tests/Tasks/diagonalize_matrix_tests.R
@@ -4,45 +4,37 @@ context("diagonalize_matrix()")
 
 test_that("Assignment: diagonalize_matrix", {
   X <- matrix(c(-1,-3,-3,3,5,3,-1,-1,1),ncol=3)
-  expect_that(exists("diagonalize_matrix"), is_true(),
+  
+  expect_true(exists("diagonalize_matrix"),
               info = "Fel: diagonalize_matrix() saknas.")
-  expect_that(diagonalize_matrix, is_a("function"),
-              info = "Fel: diagonalize_matrix ska vara en funktion.")
-  expect_function_self_contained(object = diagonalize_matrix,
-                        "Fel: Funktionen har fria variabler")
-  expect_that(all(names(formals(diagonalize_matrix)) %in% c("X")), condition=is_true(),
-              info = "Fel: Argumenten i funktionen har felaktiga namn.")
-  expect_that(class(diagonalize_matrix(X)), 
-              is_equivalent_to("list"),
-              info="Fel: Funktionen returnerar inte en lista")
-  expect_that(all(names(diagonalize_matrix(X)) %in% 
-              c("D", "P", "Pinv")), is_true(),
-              info="Fel: Funktionen returnerar inte korrekta listelementnamn.")
-  expect_that(class(diagonalize_matrix(X)$D), 
-              is_equivalent_to("matrix"),
-              info="Fel: D ska vara en matris")
-  expect_that(class(diagonalize_matrix(X)$P), 
-              is_equivalent_to("matrix"),
-              info="Fel: P ska vara en matris")
-  expect_that(class(diagonalize_matrix(X)$Pinv), 
-              is_equivalent_to("matrix"),
-              info="Fel: Pinv ska vara en matris")
-  expect_error(diagonalize_matrix(matrix(c(3,3,2,2),byrow = TRUE,ncol=2)),
+  checkmate::expect_function(diagonalize_matrix, nargs = 1)
+  expect_function_self_contained(diagonalize_matrix)
+  
+  expect_function_arguments(diagonalize_matrix, c("X"))
+  
+  checkmate::expect_class(diagonalize_matrix(X), "list")
+
+  expect_silent(diagonalize_matrix(X))
+    
+  expect_names(names(diagonalize_matrix(X)), permutation.of = c("D", "P", "Pinv"))
+  
+  checkmate::expect_class(diagonalize_matrix(X)$D, "matrix")
+  
+  checkmate::expect_class(diagonalize_matrix(X)$P, "matrix")
+  
+  checkmate::expect_class(diagonalize_matrix(X)$Pinv, "matrix")
+  
+  expect_error(diagonalize_matrix(matrix(c(3,3,2,2),byrow = TRUE, ncol=2)),
               info = "Fel: Funktionen avbryter inte for singulara matriser (det(A) = 0).")
   
   # Check that mat$P %*% mat$D %*% mat$Pinv returns the matrix
-  A <- matrix(3:6,ncol=2)
-  mat <- diagonalize_matrix(A)
-  expect_true(all(diag(round(mat$D,2)) %in% c(9.22, -0.22)), 
-              info="Fel: Berakning av D")
-  expect_true(all(round(mat$P %*% mat$D %*% mat$Pinv) == A), 
-              info="Fel: P %*% D %*% Pinv returnerar inte den ursprungliga matrisen.")  
-    
-  B <- matrix(c(-1,-3,-3,3,5,3,-1,-1,1),ncol=3)
-  mat <- diagonalize_matrix(B)
-  expect_true(all(diag(round(mat$D,2)) %in% c(2,2,1)), 
-              info="Fel: Berakning av D ar felaktig.")
-  expect_true(all(round(mat$P %*% mat$D %*% mat$Pinv) == B), 
-              info="Fel: P %*% D %*% Pinv returnerar inte den ursprungliga matrisen.")  
+  result1 <- diagonalize_matrix(matrix(3:6, ncol=2))
+  expect_equal(diag(result1$D), c(9.22, -0.22), tolerance = 0.01, info="result <- diagonalize_matrix(matrix(3:6, ncol=2))")
+  expect_equal(result1$P %*% result1$D %*% result1$Pinv, matrix(3:6, ncol=2), tolerance = 0.01, info="result <- diagonalize_matrix(matrix(3:6, ncol=2))")
 
+  B <- matrix(c(-1,-3,-3, 3, 5, 3, -1, -1, 1),ncol=3)
+  result2 <- diagonalize_matrix(B)
+  expect_equal(diag(result2$D), c(2,2,1), tolerance = 0.01, info = "D is incorrect.")
+  expect_equal(result2$P %*% result2$D %*% result2$Pinv, B, tolerance = 0.01, info="P * D * Pinv != original matrix.")
+  
 })
--- Labs/Tests/Tasks/my_grouped_test_tests.R
+++ Labs/Tests/Tasks/my_grouped_test_tests.R
@@ -5,51 +5,45 @@ context("my_grouped_test()")
 test_that("Assignment: my_grouped_test()", {
       
   # General tests
-  expect_that(exists("my_grouped_test"), is_true(),
-              info = "Fel: my_grouped_test() saknas.")
-  expect_that(my_grouped_test, is_a("function"),
-              info = "Fel: my_grouped_test är inte en funktion.")
-  expect_that(all(names(formals(my_grouped_test)) %in% c("data_vector","my_groups","alpha")), condition=is_true(),
-              info = "Fel: Namnen på argumenten i funktionen är fel.")
+  expect_true(exists("my_grouped_test"))
+  
+  checkmate::expect_function(my_grouped_test, nargs = 3)
+
+  expect_function_arguments(my_grouped_test, c("data_vector","my_groups","alpha"))
   
   data(chickwts)
-  chick <- my_grouped_test(chickwts[,1],chickwts[,2],0.05)
-  expect_is(chick, "matrix",
-            info = "Fel: Funktionen returnerar inte en matris")
-  expect_true(all(colnames(chick) %in% 
-                    c("Lower CI-limit", "Mean", "Upper CI-limit", "No of obs.")),
-              info = "Fel: Funktionen returnerar fel kolumnnamn.")
-  expect_true(all(rownames(chick) %in% 
-                    c("casein", "horsebean", "linseed", "meatmeal", "soybean", "sunflower")),
-              info = "Fel: Funktionen returnerar fel radnamn.")
-  expect_equal(dim(chick), c(6,4),
-            info = "Fel: Funktionen returnerar inte en matris av rätt storlek.")
-  expect_equal(round(chick["soybean", "Mean"], 2), 
-               246.43, info = "Fel: Funktionen returnerar fel värde på 'Mean'.")
-  expect_equal(round(chick["meatmeal", "Upper CI-limit"], 2), 
-               320.51, info = "Fel: Funktionen returnerar fel värde på 'CI-limit'.")
-  expect_equal(round(chick["meatmeal", "No of obs."], 2), 
-               11, info = "Fel: Funktionen returnerar fel värde på 'No of obs.'.")
-    
-  data(InsectSprays)
-  # Test 1 - InsectSprays data
-  chick <- my_grouped_test(InsectSprays$count,InsectSprays$spray,0.1)
-  expect_is(chick, "matrix",
-            info = "Fel: Funktionen returnerar inte en matris")
-  expect_true(all(colnames(chick) %in% 
-                    c("Lower CI-limit", "Mean", "Upper CI-limit", "No of obs.")),
-              info = "Fel: Funktionen returnerar fel kolumnnamn.")
-  expect_true(all(rownames(chick) %in% 
-                    c("A","B","C","D","E","F")),
-              info = "Fel: Funktionen returnerar fel radnamn.")
-  expect_equal(dim(chick), c(6,4),
-               info = "Fel: Funktionen returnerar inte en matris av rätt storlek.")
-  expect_equal(round(chick["B", "Mean"], 2), 
-               15.33, info = "Fel: Funktionen returnerar fel värde på 'Mean'.")
-  expect_equal(round(chick["F", "Upper CI-limit"], 2), 
-               19.89, info = "Fel: Funktionen returnerar fel värde på 'CI-limit'.")
-  expect_equal(round(chick["D", "No of obs."], 2), 
-               12, info = "Fel: Funktionen returnerar fel värde på 'No of obs.'.")
+  chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)
+  expect_class(chickwts_results, "matrix",
+               info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)")
   
+  expect_names(colnames(chickwts_results), permutation.of = c("Lower CI-limit", "Mean", "Upper CI-limit", "No of obs."),
+               info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)")
+  
+  expect_names(rownames(chickwts_results), permutation.of = c("casein", "horsebean", "linseed", "meatmeal", "soybean", "sunflower"),
+               info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)")
+
+  expect_equal(dim(chickwts_results), c(6,4),
+            info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)")
+  expect_equal(chickwts_results["soybean", "Mean"], 246.43, tolerance = 0.01, info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)") 
+  expect_equal(chickwts_results["meatmeal", "Upper CI-limit"], 320.51, tolerance = 0.01, info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)") 
+  expect_equal(chickwts_results["meatmeal", "No of obs."], 11, tolerance = 0.01, info = "chickwts_results <- my_grouped_test(chickwts[,1], chickwts[,2], 0.05)") 
+
+  data(InsectSprays)
+  insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)
+  expect_class(insect_results, "matrix",
+               info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)")
+  
+  expect_names(colnames(insect_results), permutation.of = c("Lower CI-limit", "Mean", "Upper CI-limit", "No of obs."),
+               info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)")
+  
+  expect_names(rownames(insect_results), permutation.of = c("A", "B", "C", "D", "E", "F"),
+               info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)")
+  
+  expect_equal(dim(insect_results), c(6,4),
+               info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)")
+  expect_equal(unname(insect_results[c("A", "B", "C", "D", "E", "F"), "Mean"]), c(14.5, 15.333, 2.083,  4.916,  3.5, 16.666), tolerance = 0.01, info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)") 
+  expect_equal(insect_results["F", "Upper CI-limit"], 19.89, tolerance = 0.01, info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)") 
+  expect_equal(insect_results["D", "No of obs."], 12, tolerance = 0.01, info = "insect_results <- my_grouped_test(InsectSprays$count, InsectSprays$spray, 0.1)") 
+
 })
 
